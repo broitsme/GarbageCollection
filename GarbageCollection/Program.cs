b6e624d26480ad61ceda0034793a9d8813822ba2
Rahul Bhatia <Rahul_Bhatia@epam.com>
2020-01-28 13:00:59 +0530

    Add project files.

---
+++ GarbageCollection/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace GarbageCollectionNameSpace
+{
+    public class GarbageCollection
+    {
+        public class Destructor
+        {
+            StreamReader stream = File.OpenText(@"C:\Users\Rahul_Bhatia\Downloads\desktop.ini");
+            ~Destructor() { Console.WriteLine("Destructor is Exicuted and streamReader is Disposed"); }
+        }
+        public class UsingExample
+        {
+             public void Method()
+            {
+                using (StreamReader stream = File.OpenText(@"C:\Users\Rahul_Bhatia\Downloads\desktop.ini"))
+                {
+                    Console.WriteLine("using block is exicuted and streamReader is Disposed");
+                }
+            }
+        }
+
+        public class ForDispose : IDisposable
+        {
+            bool disposed = false;
+            StreamReader stream = File.OpenText(@"C:\Users\Rahul_Bhatia\Downloads\desktop.ini"); //unmanaged code
+            public void Dispose()
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+            protected virtual void Dispose(bool disposing)
+            {
+                if (disposed)
+                    return;
+
+                if (disposing)
+                {
+                    stream.Dispose();
+                    Console.WriteLine("Disposable is exicuted and stream is Disposed!");
+                }
+
+                disposed = true;
+            }
+        }
+        public static void Main(String[] args)
+        {
+            ForDispose fd = new ForDispose();
+            fd.Dispose();
+            Destructor des = new Destructor();
+            UsingExample ue = new UsingExample();
+            ue.Method();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Runner/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GarbageCollectionNameSpace;
+namespace Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          for(int i = 0; i< 100000; i++)
+            {
+                int[] arr = new int[10000];
+            }
+            Console.WriteLine(GC.GetTotalMemory(false));
+            GC.Collect();
+            Console.WriteLine(GC.GetTotalMemory(false));
+            Console.WriteLine();
+            for (int i = 0; i < 100000; i++)
+            {
+                int[] arr = new int[10000];
+            }
+            Console.WriteLine(GC.GetTotalMemory(false));
+            GC.Collect();
+            Console.WriteLine(GC.GetTotalMemory(false));
+            Console.WriteLine(); for (int i = 0; i < 100000; i++)
+            {
+                int[] arr = new int[10000];
+            }
+            Console.WriteLine(GC.GetTotalMemory(false));
+            GC.Collect();
+            Console.WriteLine(GC.GetTotalMemory(false));
+            Console.WriteLine(); for (int i = 0; i < 100000; i++)
+            {
+                int[] arr = new int[10000];
+            }
+            Console.WriteLine(GC.GetTotalMemory(false));
+            GC.Collect();
+            Console.WriteLine(GC.GetTotalMemory(false));
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
